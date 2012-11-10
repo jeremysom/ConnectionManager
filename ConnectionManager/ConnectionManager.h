384fec77a7138d90cb9af3d61926f04e768e2197
jeremysom <jems93@gmail.com>
2012-11-10 20:57:00 +1100

    Initial Commit

---
+++ ConnectionManager/ConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.h
+//  ConnectionManager
+//
+//  Created by Jeremy Somerville on 10/11/12.
+//  Copyright (c) 2012 Jeremy Somerville. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ConnectionManager : NSObject
+
+@end
---
+++ ConnectionManager/ConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  ConnectionManager.m
+//  ConnectionManager
+//
+//  Created by Jeremy Somerville on 10/11/12.
+//  Copyright (c) 2012 Jeremy Somerville. All rights reserved.
+//
+
+#import "ConnectionManager.h"
+
+@implementation ConnectionManager
+
+@end
